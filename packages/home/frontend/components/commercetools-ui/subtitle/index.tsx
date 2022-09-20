cab41f1d90ff7f1449b6c4e9d8eeb4378360c297
Mina Badrous <minabadrous.vu@gmail.com>
2022-09-20 15:59:43 +0200

    Seperating Title and Subtitle atomic components, Creating wrapper component for inline padding

---
+++ packages/home/frontend/components/commercetools-ui/subtitle/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+type SubtitleProps = {
+  subtitle: string;
+  className?: string;
+};
+
+const Subtitle: FC<SubtitleProps> = ({ subtitle, className }) => {
+  return <p className={`${className ?? ''} text-12 font-thin leading-loose md:text-14 lg:text-16`}>{subtitle}</p>;
+};
+
+export default Subtitle;
---
+++ packages/home/frontend/components/commercetools-ui/title/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+type TitleProps = {
+  title: string;
+  className?: string;
+};
+
+const Title: FC<TitleProps> = ({ title, className }) => {
+  return <h3 className={`${className ?? ''} text-16 font-bold md:text-22 lg:text-28`}>{title}</h3>;
+};
+
+export default Title;
