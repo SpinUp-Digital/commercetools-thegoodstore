6a13a5419fc57a4852883b488cf8d14c4e0ae97b
xcintas <xcintas@gmail.com>
2022-09-30 12:42:19 +0300

    empty commit

--- packages/home/frontend/components/commercetools-ui/subtitle/index.tsx
+++ packages/home/frontend/components/commercetools-ui/subtitle/index.tsx
@@ -1,11 +1,11 @@
-import { FC } from 'react';
+import React from 'react';
 
 type SubtitleProps = {
   subtitle: string;
   className?: string;
 };
 
-const Subtitle: FC<SubtitleProps> = ({ subtitle, className }) => {
+const Subtitle: React.FC<SubtitleProps> = ({ subtitle, className }) => {
   return <p className={`${className ?? ''} text-14 font-thin leading-loose md:text-14 lg:text-16`}>{subtitle}</p>;
 };
 
