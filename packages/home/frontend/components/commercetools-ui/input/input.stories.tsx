6757b0d1e73686a63e99c2a1299b9f0ef1135f7e
abdotaker608 <abdotaker607@gmail.com>
2022-10-25 14:35:21 +0200

    feat(FP-2912): Input mini component

---
+++ packages/home/frontend/components/commercetools-ui/input/input.stories.tsx
@@ -0,0 +1,24 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import Input from '.';
+
+export default {
+  title: 'Frontastic/Input',
+  component: Input,
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} placeholder="Placeholder" label="Label" />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  variant: 'primary',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  variant: 'secondary',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+};
