e239ebb438a92f6084b162eae602159293aa0a7f
Mina Badrous <minabadrous.vu@gmail.com>
2022-11-15 11:59:32 +0200

    fix(FP-2883): PDP design bugs fixes

--- packages/home/frontend/components/commercetools-ui/organisms/products/product-details/additional-info.tsx
+++ packages/home/frontend/components/commercetools-ui/organisms/products/product-details/additional-info.tsx
@@ -43,7 +43,7 @@ const AdditionalInfo: FC<AdditionalInfoProps> = ({ productspec, description }) =
         />
       </AccordionBtn>
       <AccordionBtn
-        className="col-span-2 mt-24 border-b border-neutral-400 md:mt-0"
+        className="col-span-2 border-b border-neutral-400 md:mt-0"
         closedSectionTitle={formatMessage({ id: 'details.additional', defaultMessage: 'Additional Details' })}
         buttonClassName="py-21 font-body text-16 leading-tight text-primary-black pl-0 md:pr-12"
         panelClassName="pb-20 md:pt-10 md:pb-30 text-primary-black"
