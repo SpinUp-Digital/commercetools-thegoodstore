229c285364f1126ddd9ac9d657e48a2e209b2529
abdotaker608 <abdotaker607@gmail.com>
2022-11-16 11:52:06 +0200

    fix(FP-3162): Category slider loop removed

--- packages/home/frontend/components/commercetools-ui/organisms/category-slider/index.tsx
+++ packages/home/frontend/components/commercetools-ui/organisms/category-slider/index.tsx
@@ -1,81 +1,44 @@
 import React, { useMemo } from 'react';
-import useImageSizes from 'helpers/hooks/useImageSizes';
 import useMediaQuery from 'helpers/hooks/useMediaQuery';
 import { desktop, tablet } from 'helpers/utils/screensizes';
-import { Reference } from 'types/reference';
-import Image, { NextFrontasticImage } from 'frontastic/lib/image';
-import Link from '../../atoms/link';
 import Slider from '../../atoms/slider';
-import Typography from '../../atoms/typography';
 import Wrapper from '../content/wrapper';
+import Tile, { Props as TileProps } from './tile';
 
 export interface Props {
-  tiles: Array<{
-    title: string;
-    image: NextFrontasticImage;
-    target?: Reference;
-  }>;
+  tiles: Array<TileProps['tile']>;
 }
 
 const CategorySlider: React.FC<Props> = ({ tiles = [] }) => {
   const [isDesktopSize] = useMediaQuery(desktop);
-
-  const tileImageSizes = useImageSizes({ md: 0.5, lg: 0.25, defaultSize: 0.25 });
 
   const fitsToScreenSize = useMemo(
     () => (isDesktopSize && tiles.length <= 4) || (!isDesktopSize && tiles.length <= 2),
     [isDesktopSize, tiles.length],
   );
 
-  const Tiles = useMemo(() => {
-    return tiles.map((tile, index) => (
-      <Link key={index} link={tile.target} className="block shrink-0 grow basis-0">
-        <div className="relative h-[160px] sm:h-[256px] md:h-[356px]">
-          <Image
-            {...tile.image}
-            sizes={tileImageSizes}
-            alt={tile.title}
-            layout="fill"
-            objectFit="cover"
-            className="brightness-75"
-            loading="eager"
-          />
-        </div>
-        <Typography
-          as="h6"
-          fontSize={14}
-          align="center"
-          className="mt-5 block overflow-hidden truncate rounded-b-sm border-neutral-300 py-5 md:text-16 lg:mt-0 lg:border lg:bg-white lg:py-10"
-        >
-          {tile.title}
-        </Typography>
-      </Link>
-    ));
-  }, [tiles]);
-
   return (
     <Wrapper background="neutral-200">
-      {fitsToScreenSize ? (
-        <div className="justify-stretch flex gap-4 md:gap-8 lg:gap-16">{Tiles}</div>
-      ) : (
-        <Slider
-          slidesPerView={isDesktopSize ? 4 : 2.3}
-          dots={false}
-          spaceBetween={4}
-          arrows={isDesktopSize && tiles.length > 4}
-          loop
-          breakpoints={{
-            [tablet]: {
-              spaceBetween: 8,
-            },
-            [desktop]: {
-              spaceBetween: 16,
-            },
-          }}
-        >
-          {Tiles}
-        </Slider>
-      )}
+      <Slider
+        slidesPerView={2.3}
+        dots={false}
+        spaceBetween={4}
+        arrows={isDesktopSize && !fitsToScreenSize}
+        allowTouchMove={!fitsToScreenSize}
+        breakpoints={{
+          [tablet]: {
+            spaceBetween: 8,
+          },
+          [desktop]: {
+            spaceBetween: 16,
+            slidesPerView: 4,
+          },
+        }}
+      >
+        {tiles.map((tile, index) => (
+          <Tile key={index} tile={tile} />
+        ))}
+      </Slider>
     </Wrapper>
   );
 };
---
+++ packages/home/frontend/components/commercetools-ui/organisms/category-slider/tile.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Reference } from 'types/reference';
+import Image, { NextFrontasticImage } from 'frontastic/lib/image';
+import Link from '../../atoms/link';
+import Typography from '../../atoms/typography';
+import useImageSizes from 'helpers/hooks/useImageSizes';
+
+export interface Props {
+  tile: {
+    title: string;
+    image: NextFrontasticImage;
+    target?: Reference;
+  };
+}
+
+const Tile: React.FC<Props> = ({ tile }) => {
+  const tileImageSizes = useImageSizes({ md: 0.5, lg: 0.25, defaultSize: 0.25 });
+
+  return (
+    <Link link={tile.target} className="block shrink-0 grow basis-0">
+      <div className="relative h-[160px] sm:h-[256px] md:h-[356px]">
+        <Image
+          {...tile.image}
+          sizes={tileImageSizes}
+          alt={tile.title}
+          layout="fill"
+          objectFit="cover"
+          className="brightness-75"
+          loading="eager"
+        />
+      </div>
+      <Typography
+        as="h6"
+        fontSize={14}
+        align="center"
+        className="mt-5 block overflow-hidden truncate rounded-b-sm border-neutral-300 py-5 md:text-16 lg:mt-0 lg:border lg:bg-white lg:py-10"
+      >
+        {tile.title}
+      </Typography>
+    </Link>
+  );
+};
+
+export default Tile;
