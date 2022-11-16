cf1ca6ace367576a607d6e8ddd46f67cd830d545
abdotaker608 <abdotaker607@gmail.com>
2022-11-16 08:43:21 +0200

    fix(FP-3162): Category slider loop bug:

--- packages/home/frontend/components/commercetools-ui/organisms/category-slider/index.tsx
+++ packages/home/frontend/components/commercetools-ui/organisms/category-slider/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import useImageSizes from 'helpers/hooks/useImageSizes';
 import useMediaQuery from 'helpers/hooks/useMediaQuery';
 import { desktop, tablet } from 'helpers/utils/screensizes';
@@ -22,51 +22,60 @@ const CategorySlider: React.FC<Props> = ({ tiles = [] }) => {
 
   const tileImageSizes = useImageSizes({ md: 0.5, lg: 0.25, defaultSize: 0.25 });
 
+  const fitsToScreenSize = useMemo(
+    () => (isDesktopSize && tiles.length <= 4) || (!isDesktopSize && tiles.length <= 2),
+    [isDesktopSize, tiles.length],
+  );
+
+  const Tiles = useMemo(() => {
+    return tiles.map((tile, index) => (
+      <Link key={index} link={tile.target} className="block shrink-0 grow basis-0">
+        <div className="relative h-[160px] sm:h-[256px] md:h-[356px]">
+          <Image
+            {...tile.image}
+            sizes={tileImageSizes}
+            alt={tile.title}
+            layout="fill"
+            objectFit="cover"
+            className="brightness-75"
+            loading="eager"
+          />
+        </div>
+        <Typography
+          as="h6"
+          fontSize={14}
+          align="center"
+          className="mt-5 block overflow-hidden truncate rounded-b-sm border-neutral-300 py-5 md:text-16 lg:mt-0 lg:border lg:bg-white lg:py-10"
+        >
+          {tile.title}
+        </Typography>
+      </Link>
+    ));
+  }, [tiles]);
+
   return (
     <Wrapper background="neutral-200">
-      <Slider
-        slidesPerView={2.3}
-        dots={false}
-        spaceBetween={4}
-        arrows={isDesktopSize && tiles.length > 4}
-        allowTouchMove
-        loop
-        breakpoints={{
-          [tablet]: {
-            spaceBetween: 8,
-          },
-          [desktop]: {
-            allowTouchMove: tiles.length > 4,
-            loop: tiles.length > 4,
-            spaceBetween: 16,
-            slidesPerView: 4,
-          },
-        }}
-      >
-        {tiles.map((tile, index) => (
-          <Link key={index} link={tile.target} className="block">
-            <div className="relative h-[160px] sm:h-[256px] md:h-[356px]">
-              <Image
-                {...tile.image}
-                sizes={tileImageSizes}
-                alt={tile.title}
-                layout="fill"
-                objectFit="cover"
-                className="brightness-75"
-                loading="eager"
-              />
-            </div>
-            <Typography
-              as="h6"
-              fontSize={14}
-              align="center"
-              className="mt-5 block overflow-hidden truncate rounded-b-sm border-neutral-300 py-5 md:text-16 lg:mt-0 lg:border lg:bg-white lg:py-10"
-            >
-              {tile.title}
-            </Typography>
-          </Link>
-        ))}
-      </Slider>
+      {fitsToScreenSize ? (
+        <div className="justify-stretch flex gap-4 md:gap-8 lg:gap-16">{Tiles}</div>
+      ) : (
+        <Slider
+          slidesPerView={isDesktopSize ? 4 : 2.3}
+          dots={false}
+          spaceBetween={4}
+          arrows={isDesktopSize && tiles.length > 4}
+          loop
+          breakpoints={{
+            [tablet]: {
+              spaceBetween: 8,
+            },
+            [desktop]: {
+              spaceBetween: 16,
+            },
+          }}
+        >
+          {Tiles}
+        </Slider>
+      )}
     </Wrapper>
   );
 };
