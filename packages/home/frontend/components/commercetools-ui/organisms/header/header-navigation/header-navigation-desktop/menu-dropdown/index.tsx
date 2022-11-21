1133c5221241a4d96d412b9c5eb34fed8ac8f143
KenwayTheGreat <ahmad_amir@live.com>
2022-11-21 11:58:17 +0200

    Fix (FP-3163): Main Navigation Component - https://frontastic.atlassian.net/browse/FP-3164 - https://frontastic.atlassian.net/browse/FP-3165 - https://frontastic.atlassian.net/browse/FP-3166 - https://frontastic.atlassian.net/browse/FP-3167 - https://frontastic.atlassian.net/browse/FP-3168 - https://frontastic.atlassian.net/browse/FP-3175 - https://frontastic.atlassian.net/browse/FP-3194

--- packages/home/frontend/components/commercetools-ui/organisms/header/header-navigation/header-navigation-desktop/menu-dropdown/index.tsx
+++ packages/home/frontend/components/commercetools-ui/organisms/header/header-navigation/header-navigation-desktop/menu-dropdown/index.tsx
@@ -21,28 +21,25 @@ const MenuDropdown: FC<Props> = ({ links, tileContent, market, onClick }) => {
     'border-b-[1.5px] border-t-[1.5px] border-b-secondary-grey border-t-neutral-400 bg-white px-50 py-34 xl:pl-143 xl:pr-195',
   ]);
   const linksClassNames = useClassNames([
-    'w-[65%] justify-between lg:grid lg:grid-cols-2 xl:flex',
+    'w-[65%] justify-left lg:grid lg:grid-cols-2 xl:flex',
     links?.length > 4 ? 'xl:grid xl:grid-cols-2' : '',
     tileContent ? 'pr-25' : '',
   ]);
+  const linkClassNames = useClassNames(['w-fit whitespace-nowrap', links.length < 4 ? 'pr-116' : 'pr-75']);
   return (
     <div className={wrapperClassNames}>
       <div className={linksClassNames}>
         {links?.map((link) => (
-          <div key={link.categoryId} className="w-185 lg:w-200">
+          <div key={link.categoryId} className={linkClassNames}>
             {link.depth === 1 ? (
               <div key={link.categoryId}>
-                <div>
-                  <Link
-                    link={link.slug ?? link.path}
-                    variant="menu-header"
-                    className="w-3/4 overflow-visible xl:w-full"
-                  >
+                <div className="w-fit pb-4">
+                  <Link link={link.slug ?? link.path} variant="menu-header">
                     <Typography>{link.name}</Typography>
                   </Link>
                 </div>
                 {link.subCategories.map((field) => (
-                  <div key={field.categoryId} onClick={onClick} className="my-4">
+                  <div key={field.categoryId} onClick={onClick} className="py-4">
                     <Link link={field.slug ?? field.path} className="h-22 w-fit" variant="menu-item">
                       <Typography>{field.name}</Typography>
                     </Link>
---
+++ packages/home/frontend/helpers/hooks/useResizeObserver.ts
@@ -0,0 +1,19 @@
+import React, { useCallback, useEffect } from 'react';
+
+const useResizeObserver = (ref: React.RefObject<HTMLElement>, callback: (entry?: ResizeObserverEntry) => void) => {
+  const resizeCallback = useCallback(
+    (entries: ResizeObserverEntry[]) => {
+      callback(entries[0]);
+    },
+    [callback],
+  );
+
+  useEffect(() => {
+    callback();
+    const observer = new ResizeObserver(resizeCallback);
+    observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, [ref, callback, resizeCallback]);
+};
+
+export default useResizeObserver;
