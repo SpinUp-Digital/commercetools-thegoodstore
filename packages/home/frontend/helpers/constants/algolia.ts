ad7e20f600e2f7446639c9a559927a652c32045e
xcintas <xcintas@gmail.com>
2023-01-06 13:18:16 +0200

    hardcoded product index

--- packages/home/frontend/helpers/constants/algolia.ts
+++ packages/home/frontend/helpers/constants/algolia.ts
@@ -1,1 +1,1 @@
-export const productsIndex = process.env.NEXT_PUBLIC_ALGOLIA_PRODUCTS_INDEX as string;
+export const productsIndex = process.env.NEXT_PUBLIC_ALGOLIA_PRODUCTS_INDEX as string || 'test_TheGoodStore';
