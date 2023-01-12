125160dc7bb1921b8e16dd278ab9700f2cad3cca
abdotaker608 <abdotaker607@gmail.com>
2023-01-12 11:39:28 +0200

    Fix overlay bg opacity

--- packages/home/frontend/components/commercetools-ui/atoms/overlay/index.tsx
+++ packages/home/frontend/components/commercetools-ui/atoms/overlay/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const Overlay = () => {
-  return <div className="fixed top-0 left-0 z-[250] h-screen w-screen bg-[#7f7f7f]" />;
+  //eslint-disable-next-line tailwindcss/no-custom-classname
+  return <div className="fixed top-0 left-0 z-[250] h-screen w-screen bg-[#7f7f7f]/30" />;
 };
 
 export default Overlay;
