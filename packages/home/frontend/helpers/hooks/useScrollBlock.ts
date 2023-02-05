dea53859e1f21ef51be3f095184ba6028cb47857
abdotaker608 <abdotaker607@gmail.com>
2023-02-05 13:06:00 +0200

    fix(FP-3645): Refactor scrollbar block hook

--- packages/home/frontend/helpers/hooks/useScrollBlock.ts
+++ packages/home/frontend/helpers/hooks/useScrollBlock.ts
@@ -1,54 +1,21 @@
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 
 const useScrollBlock = () => {
-  const scroll = useRef(false);
   const [isBlocked, setIsBlocked] = useState<boolean>(false);
 
   const blockScrolling = () => {
-    if (typeof document === 'undefined') return;
-
-    const html = document.documentElement;
-    const { body } = document;
-
-    if (!body || !body.style || scroll.current) return;
-
-    /**
-     * 1. Fixes a bug in iOS and desktop Safari whereby setting
-     *    `overflow: hidden` on the html/body does not prevent scrolling.
-     * 2. Fixes a bug in desktop Safari where `overflowY` does not prevent
-     *    scroll if an `overflow-x` style is also applied to the body.
-     */
-    html.style.position = 'relative'; /* [1] */
-    body.style.position = 'relative'; /* [1] */
-    html.style.overflow = 'hidden'; /* [2] */
-    body.style.overflow = 'hidden'; /* [2] */
-
-    scroll.current = true;
+    document.body.style.position = 'fixed';
+    document.body.style.overflowY = 'scroll';
   };
 
   const allowScrolling = () => {
-    if (typeof document === 'undefined') return;
-
-    const html = document.documentElement;
-    const { body } = document;
-
-    if (!body || !body.style || !scroll.current) return;
-
-    html.style.position = '';
-    html.style.overflow = '';
-    body.style.position = '';
-    body.style.overflow = '';
-    body.style.paddingRight = '';
-
-    scroll.current = false;
+    document.body.style.position = 'static';
+    document.body.style.overflowY = 'auto';
   };
 
   const blockScroll = (status: boolean) => {
-    if (status) {
-      blockScrolling();
-    } else {
-      allowScrolling();
-    }
+    if (status) blockScrolling();
+    else allowScrolling();
 
     setIsBlocked(status);
   };
