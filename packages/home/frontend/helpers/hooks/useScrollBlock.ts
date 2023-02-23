41f9f01a994fa967d2ac34ba1a42fad2fdcfa1c7
minabadrous <minabadrous.vu@gmail.com>
2023-02-23 12:30:31 +0200

    Added header padding on locking scroll to avoid page shift

--- packages/home/frontend/helpers/hooks/useScrollBlock.ts
+++ packages/home/frontend/helpers/hooks/useScrollBlock.ts
@@ -6,11 +6,15 @@ const useScrollBlock = () => {
   const [isBlocked, setIsBlocked] = useState<boolean>(false);
   const { isTouchDevice } = useTouchDevice();
 
+  const header = document.querySelector('header') as HTMLDivElement;
+
   const blockScrolling = () => {
+    const header = document.querySelector('header') as HTMLDivElement;
     const scrollbarWidth = getScrollbarWidth();
 
     if (!isTouchDevice) {
       document.body.style.paddingRight = `${scrollbarWidth}px`;
+      header.style.paddingRight = `${scrollbarWidth}px`;
     }
 
     document.body.style.overflowY = 'hidden';
@@ -18,6 +22,7 @@ const useScrollBlock = () => {
 
   const allowScrolling = () => {
     document.body.style.paddingRight = '0px';
+    header.style.paddingRight = '0px';
     document.body.style.overflowY = 'auto';
   };
 
