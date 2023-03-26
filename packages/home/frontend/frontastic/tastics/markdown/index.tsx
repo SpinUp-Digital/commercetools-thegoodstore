51e87f6e9d507681c00e6394dde42723ef09b737
abdotaker608 <abdotaker607@gmail.com>
2023-03-26 09:36:45 +0200

    fix(FP-4002): Markdown fullwidth option

--- packages/home/frontend/frontastic/tastics/markdown/index.tsx
+++ packages/home/frontend/frontastic/tastics/markdown/index.tsx
@@ -1,12 +1,15 @@
 import Markdown, { Props as MarkdownProps } from 'components/commercetools-ui/organisms/markdown';
+import useClassNames from 'helpers/hooks/useClassNames';
 
 interface Props {
-  data: MarkdownProps;
+  data: MarkdownProps & { fullWidth: boolean };
 }
 
-const MarkdownTastic = ({ data }: Props) => {
+const MarkdownTastic = ({ data: { fullWidth = false, ...data } }: Props) => {
+  const classNames = useClassNames(['prose p-24 md:p-56 lg:p-84', !fullWidth ? 'max-w-[1024px]' : 'max-w-full']);
+
   return (
-    <div className="prose max-w-[1024px] p-24 md:p-56 lg:p-84">
+    <div className={classNames}>
       <Markdown {...data} />
     </div>
   );
