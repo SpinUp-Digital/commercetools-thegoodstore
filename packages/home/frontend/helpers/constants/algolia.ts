721b22f59f6e98a1bb293cec3548c7c13d8276ec
xcintas <xcintas@gmail.com>
2023-04-07 12:37:00 +0300

    algolia fix

--- packages/home/frontend/helpers/constants/algolia.ts
+++ packages/home/frontend/helpers/constants/algolia.ts
@@ -1,2 +1,2 @@
-export const productsIndex = process.env.NEXT_PUBLIC_ALGOLIA_PRODUCTS_INDEX;
-export const productsQuerySuggestionsIndex = process.env.NEXT_PUBLIC_ALGOLIA_PRODUCTS_QUERY_SUGGESTIONS_INDEX;
+export const productsIndex = process.env.NEXT_PUBLIC_ALGOLIA_PRODUCTS_INDEX || '';
+export const productsQuerySuggestionsIndex = process.env.NEXT_PUBLIC_ALGOLIA_PRODUCTS_QUERY_SUGGESTIONS_INDEX || '';
