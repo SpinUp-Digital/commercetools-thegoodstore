e874fd1b09b64c618fa2acef7252bd18a19abd21
KenwayTheGreat <ahmad_amir@live.com>
2023-06-05 15:43:58 +0300

    Fix (FP-4348): Fixed the left menu for account

--- packages/home/frontend/components/commercetools-ui/organisms/account/sections/addresses/index.tsx
+++ packages/home/frontend/components/commercetools-ui/organisms/account/sections/addresses/index.tsx
@@ -75,7 +75,7 @@ const Addresses = () => {
           })}
         </Typography>
         <Link
-          link="#edit-address"
+          link="#addresses/address-add"
           className="block w-full rounded-md bg-gray-700 px-16 py-8 text-center text-14 font-medium leading-[114%] text-neutral-150 hover:cursor-pointer md:w-fit 2xl:px-40 2xl:py-12"
         >
           {formatAccountMessage({ id: 'address.add', defaultMessage: 'Add an address' })}
