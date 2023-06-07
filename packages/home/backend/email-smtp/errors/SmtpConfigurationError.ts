97745eed2d4e693e57eba0b601e223dabc88f649
KenwayTheGreat <ahmad_amir@live.com>
2023-06-07 15:26:58 +0300

    FP-4502: Removed Contentful and SMTP

--- packages/home/backend/email-smtp/errors/SmtpConfigurationError.ts
+++
@@ -1,8 +0,0 @@
-import { ErrorProps, ExtensionError } from '../../commerce-commercetools/utils/Errors';
-
-export class SmtpConfigurationError extends ExtensionError {
-  constructor(options: ErrorProps) {
-    super(options);
-    this.code = 'smtp_configuration_error';
-  }
-}
